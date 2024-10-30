386916c512038f1f3487777db9f854c3ff0c5760
Evgeny_Borisov <bsevgeny@gmail.com>
2024-10-30 05:25:38 -0500

    home work 4

--- src/main/java/spring/deserve/it/infra/SpyderCountEnvironmentPostProcessor.java
+++ src/main/java/spring/deserve/it/infra/SpyderCountEnvironmentPostProcessor.java
@@ -18,15 +18,14 @@ import java.util.Set;
 public class SpyderCountEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     private static final String GOD_MOD_PROPERTY_NAME = "GodMod";
-    private static final String SPYDER_PACKAGE = "spring.deserve.it"; // Укажите пакет, в котором находятся классы Spyder
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         // Используем Reflections для поиска классов, реализующих интерфейс Spyder
-        Reflections reflections = new Reflections(SPYDER_PACKAGE);
+        Reflections reflections = new Reflections(application.getMainApplicationClass().getPackageName());
         Set<Class<? extends Spider>> spyderClasses = reflections.getSubTypesOf(Spider.class);
 
-        if (spyderClasses.size() > 5) {
+        if (spyderClasses.size() > 2) {
             // Если количество классов больше 5, добавляем свойство GodMod=true
             Map<String, Object> godModProps = new HashMap<>();
             godModProps.put(GOD_MOD_PROPERTY_NAME, "true");
--- src/main/java/spring/deserve/it/infra/aop/AspectConfig.java
+++ src/main/java/spring/deserve/it/infra/aop/AspectConfig.java
@@ -2,10 +2,12 @@ package spring.deserve.it.infra.aop;
 
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(name = "GodMod",havingValue = "true")
 public class AspectConfig {
 
     @Bean
